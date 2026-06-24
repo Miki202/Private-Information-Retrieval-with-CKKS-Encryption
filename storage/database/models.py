6f71d8c4d9e1b979763cedb9ab4035af89963ef4
Mihaela Paparinova <mihaela_paparinova@epam.com>
2026-06-24 19:05:11 +0300

    feat: add populate db file

--- storage/database/models.py
+++ storage/database/models.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, Integer, String, DateTime, LargeBinary
 from datetime import datetime
-from connection import Base
+from storage.database.connection import Base
 
 
 class Vehicle(Base):
